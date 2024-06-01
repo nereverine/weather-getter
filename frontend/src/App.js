7e16ba5130c7de29be6717ee032b9378b29d03bb
nereverine <74254439+nereverine@users.noreply.github.com>
2024-06-01 18:18:01 +0100

    Router Creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,157 +4,88 @@ import ApplicationLogo from './components/ApplicationLogo';
 import { createTheme, MantineProvider, Grid, Card, Image, Text, Badge, Button, Group } from '@mantine/core';
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import ThermostatIcon from '@mui/icons-material/Thermostat';
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import WeatherDetails from "./components/WeatherDetails"
 
-async function getWeather(){
-  try{
-    const response = await axios.get("http://localhost:8080/pingy")
-    console.log(response);
-  }catch(error){
-    console.log(error);
-    alert("Error loading data")
-  }
+
+const getDate = (epochTime) => {
+  const date = new Date(epochTime*1000);
+  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
 }
 
 
-
-
 function App() {
-
-  
+  const [weatherData, setWeatherData] = useState([])
+  async function getWeather(){
+    try{
+      const response = await axios.get("http://localhost:8080/weathers")
+      setWeatherData(Object.values(response.data))
+      console.log(response);
+    }catch(error){
+      console.log(error);
+      alert("Error loading data")
+    }
+  }
 
 useEffect(()=> {
   getWeather();
-})
+}, []);
 
   return (
+    <BrowserRouter>
     <MantineProvider>
       <ApplicationLogo/>
-      <Grid justify='center' align='center'>
-      <Grid.Col span={2}>
+      <Grid justify='center' align='center'>  
+    {weatherData.map(function(city){
+      return(
+        <Grid.Col span={2} key={city.id}>
       <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
         <Image
-          src="/CityImages/lisbon.jpg"
+          src={`/CityImages/${city.name.toLowerCase()}.jpg`}
           height={160}
         />
       </Card.Section>
-
       <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Lisbon</Text>
-        <Badge color="pink">On Sale</Badge>
+        <Text fw={500}>{city.name}</Text>
+        <Badge color="pink" size="xl">{Math.round(city.main.temp)}ºC</Badge>
       </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
+      <Text size='sm' c="dimmed">Last Updated At: {getDate(city.dt)}</Text>
+      <Grid justify='center' align='center'>  
+      <Grid.Col span={6}>
+      <Image
+        src={`https://openweathermap.org/img/wn/${city.weather[0].icon}.png`}
+        style={{width:50}}/>
+      </Grid.Col>
+      <Grid.Col span={6}>
+      <Text>{city.weather[0].description.charAt(0).toUpperCase()+city.weather[0].description.slice(1)}</Text>
+      </Grid.Col>
+      </Grid>
+      <Text>
+       Feels like {Math.round(city.main.feels_like)}ºC
       </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
+    
+      <Button component={Link} to={`/details/${city.id}`}color="blue" fullWidth mt="md" radius="md">
+        Details
       </Button>
+  
+      
     </Card>
       </Grid.Col>
-      <Grid.Col span={2}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="/CityImages/Leiria.jpg"
-          height={160}
-        />
-      </Card.Section>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Leiria</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
-      </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
-      </Grid.Col>
-      <Grid.Col span={2}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="/CityImages/Coimbra.jpg"
-          height={160}
-        />
-      </Card.Section>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Coimbra</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
-      </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
-      </Grid.Col>
-      <Grid.Col span={2}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="/CityImages/Porto.jpg"
-          height={160}
-        />
-      </Card.Section>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Porto</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
-      </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
-      </Grid.Col>
-      <Grid.Col span={2}>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="/CityImages/faro.jpg"
-          height={160}
-        />
-      </Card.Section>
-
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Faro</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
-
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
-        activities on and around the fjords of Norway
-      </Text>
-
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
-      </Grid.Col>
+        
+      )
+    })}
     </Grid>
-    
-    
     </MantineProvider>
+    <Routes>
+      <Route path='/details/:cityId' element={<WeatherDetails/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    
+
   );
+ 
 }
 
 export default App;
---
+++ frontend/src/components/WeatherDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+function WeatherDetails({cityId}) {
+
+return(
+
+    <h1>welcome to {cityId}</h1>
+
+
+)
+
+}
+
+export default WeatherDetails;
