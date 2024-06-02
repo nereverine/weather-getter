ed669c51fd59e22e6a6e6cb0cfd60e203f9d91a2
nereverine <74254439+nereverine@users.noreply.github.com>
2024-06-02 23:21:50 +0100

    Added Daily Summary and other info to details page

--- frontend/src/components/WeatherDetails.jsx
+++ frontend/src/components/WeatherDetails.jsx
@@ -1,27 +1,74 @@
 import {React, useEffect, useState} from "react"
 import ApplicationLogo from "./ApplicationLogo";
 import { useParams } from "react-router-dom";
-import {MantineProvider, Grid, Title, Card, Paper, Text, Button} from "@mantine/core";
+import {MantineProvider, Grid, Title, Card, Paper, Text, Button, GridCol, Image, Badge} from "@mantine/core";
 import axios from "axios";
-
-
+import UploadIcon from '@mui/icons-material/Upload';
+import DownloadIcon from "@mui/icons-material/Download";
+import WaterDropIcon from '@mui/icons-material/WaterDrop';
+import CompressIcon from '@mui/icons-material/Compress';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import AirIcon from '@mui/icons-material/Air';
+import ThermostatIcon from '@mui/icons-material/Thermostat';
 
 
 function WeatherDetails() {
     const {cityName} = useParams(); // receives params of the router
-    const [weatherData, setWeatherData] = useState({})
+    const [weatherData, setWeatherData] = useState(null)
 
-    const getDate = (epochTime) => {
+    const getDateHours = (epochTime) => {
         const date = new Date(epochTime*1000);
         return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
+      }
+    const getDate = (epochTime) =>   {
+      const date = new Date(epochTime*1000)
+      return date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+    }
+
+      const getDateSun = (epochTime) =>{
+        const date = new Date(epochTime*1000);
+        return date.getHours().toString().padStart(2,'0')+":"+date.getMinutes().toString().padStart(2,'0')
+      }
+
+      const getWindDirection = (degrees) => { //converts degrees to cardinal direction
+        if (degrees >= 0 && degrees <= 44) {
+          return "North";
+        } else if (degrees >= 45 && degrees <= 89) {
+          return "Northeast";
+        } else if (degrees >= 90 && degrees <= 134) {
+          return "East";
+        } else if (degrees >= 135 && degrees <= 179) {
+          return "Southeast";
+        } else if (degrees >= 180 && degrees <= 224) {
+          return "South";
+        } else if (degrees >= 225 && degrees <= 269) {
+          return "Southwest";
+        } else if (degrees >= 270 && degrees <= 314) {
+          return "West";
+        } else if (degrees >= 315 && degrees <= 359) {
+          return "Northwest";
+        }
+      }
+      const getUVColor = (index) => { //sets color of the badge based on the index
+        if(index>=0 && index<3){
+          return "green"
+        }else if(index>=3 && index<6){
+          return "yellow"
+        }else if(index>=6 && index<8){
+          return "orange"
+        }else if(index>=8 && index<11){
+          return "red"
+        }else{
+          return "purple"
+        }
       }
 
 
     async function getWeather(){
         try{
           const response = await axios.get("http://localhost:8080/weatherById/"+cityName)
-          setWeatherData(Object.values(response.data))
-          console.log(response);
+          setWeatherData(response.data)
         }catch(error){
           console.log(error);
           alert("Error loading data")
@@ -36,6 +83,12 @@ function WeatherDetails() {
         console.log(weatherData)
       }
     
+      if(!weatherData){
+        return(
+          <></>
+        )
+      }
+
 return(
     <MantineProvider>
       <main>
@@ -43,20 +96,88 @@ return(
 <Title order={1} style={{textAlign:"center", marginBottom:10}}>{cityName}</Title>
         <Grid justify='center' align='center' style={{ width: '100%' }}>
           <Grid.Col span={4}>
-          <Paper shadow="xl" withBorder p={2}>
-          <Text size='sm' c="dimmed">Last Updated At: {getDate(weatherData[4].dt)}</Text>
-      <Title order={2}>Current Weather</Title>
-      
+          <Paper shadow="xl" withBorder p={2} style={{minHeight:150}}>
+          <Text size='sm' c="dimmed">Last Updated At: {getDateHours(weatherData.current.dt)}</Text>
+          <Grid justify='center' align='center' style={{ width: '100%' }}>
+          <Grid.Col span={6}>
+            <Title>{Math.round(weatherData.current.temp)}ºC</Title>
+      Feels Like: {Math.round(weatherData.current.feels_like)}ºC
+      </Grid.Col>
+      <GridCol span={6} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        
+      <Image 
+        src={`https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`}
+        style={{width:50}}/>
+        
+        <Text style={{fontSize:25}} align="center">{weatherData.current.weather[0].description.charAt(0).toUpperCase()+weatherData.current.weather[0].description.slice(1)}</Text>
+      </GridCol>
+      </Grid>
     </Paper>
           </Grid.Col>
           <Grid.Col span={4}>
-            <div style={{ textAlign: 'center' }}>2</div>
+            <Paper shadow="xl" withBorder p={2} style={{minHeight:150}}>
+              <Title>Daily Summary</Title>
+              <Title order={5}>{getDate(weatherData.daily[0].dt)}</Title>
+              <Grid justify="center" align="center">
+                <Grid.Col span={6}>
+              <UploadIcon style={{color:"orange"}}/> {getDateSun(weatherData.current.sunrise)}
+              <DownloadIcon style={{color:"orange"}}/>{getDateSun(weatherData.current.sunset)}
+              </Grid.Col>
+              <Grid.Col span={6}>
+              {weatherData.daily[0].summary}
+              </Grid.Col>
+              <Grid.Col span={6}>
+              <ThermostatIcon style={{color:"lightblue", verticalAlign:"middle"}}/>{Math.round(weatherData.daily[0].temp.min)}ºC
+              <ThermostatIcon style={{color:"red", verticalAlign:"middle"}}/>{Math.round(weatherData.daily[0].temp.max)}ºC
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <Grid>
+                  <Grid.Col span={6}>
+                <Text size="lg">Day: {Math.round(weatherData.daily[0].temp.day)}ºC</Text>
+                </Grid.Col>
+                <Grid.Col span={6}>
+                <Text size="lg">Night: {Math.round(weatherData.daily[0].temp.night)}ºC</Text>
+                </Grid.Col>
+                </Grid>
+              </Grid.Col>
+              </Grid>
+            </Paper>
           </Grid.Col>
           <Grid.Col span={4}>
-            <div style={{ textAlign: 'center' }}>3</div>
+            <Paper shadow="xl" withBorder p={10} style={{minHeight:150}}>
+            <Grid>
+              <GridCol span={6}>
+              <WaterDropIcon style={{fontSize:30, verticalAlign:"middle"}}/>Humidity: {weatherData.current.humidity}%
+              </GridCol>
+              <GridCol span={6} >
+              <AirIcon style={{fontSize:30, verticalAlign:"middle"}}/>Wind: {weatherData.current.wind_speed}m/s {getWindDirection(weatherData.current.wind_deg)}
+              </GridCol>
+              <GridCol span={6}>
+              <CompressIcon style={{fontSize:30, verticalAlign:"middle"}}/>Pressure: {weatherData.current.pressure}hPa
+              </GridCol>
+              <GridCol span={6} >
+              <WbSunnyIcon style={{fontSize:30, verticalAlign:"middle"}}/>UV Index: <Badge color={getUVColor(weatherData.current.uvi)}>{Math.round(weatherData.current.uvi)}</Badge>
+              </GridCol>
+              <GridCol span={6} >
+              <VisibilityIcon style={{fontSize:30, verticalAlign:"middle"}}/>Visibility: {weatherData.current.visibility/1000}km
+              </GridCol>
+            </Grid>
+            </Paper>
           </Grid.Col>
         </Grid>
         <Button onClick={teste}>teste</Button>
+        <Grid>
+          <Grid.Col span={6}>
+            <Paper shadow="xl" withBorder p={10}>
+              <Title order={2}>Hourly</Title>
+            </Paper>
+          </Grid.Col>
+          <Grid.Col span={6}>
+          <Paper shadow="xl" withBorder p={10}>
+          <Title order={2}>Forecast</Title>
+            </Paper>
+          </Grid.Col>
+        </Grid>
       </main>
     </MantineProvider>
 
